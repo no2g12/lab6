ffa33679168fa955526511cf8e25f483ae3c6d2f
no2g12 <no2g12@soton.ac.uk>
2013-11-14 14:59:21 +0000

    printed the post information

--- process-bid.php
+++ process-bid.php
@@ -1,3 +1,14 @@
 <!-- ============================== -->
 <!-- PART 1 PROCESS BID PLACEHOLDER -->
 <!-- ============================== -->
+<html>
+<body>
+
+Your name is <?php  
+  $name = $_POST["name"];
+  print_r($name); ?>
+  <br>
+Your bid is : <?php 
+  $bid = $_POST["bid"];
+  print_r($bid);
+?>
